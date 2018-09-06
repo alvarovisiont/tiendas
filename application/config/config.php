73d19d9e4f31332df217714b487a8f68e015738a
sheylert <sheylert@gmail.com>
2018-09-06 18:43:23 -0400

    cambios

--- application/config/config.php
+++ application/config/config.php
@@ -37,6 +37,8 @@ $config['base_url'] = 'https://web21facturacion.herokuapp.com/';
 */
 $config['index_page'] = '';
 
+
+
 /*
 |--------------------------------------------------------------------------
 | URI PROTOCOL
@@ -370,7 +372,7 @@ $config['encryption_key'] = 'a8f5f167f44f4964e6c998dee827110c ';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
