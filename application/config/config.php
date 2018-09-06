9f7829cfe396ff72bf52e9b39f11e94c7f422f2c
sheylert <sheylert@gmail.com>
2018-09-06 17:52:25 -0400

    cambios

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/tiendas';
+$config['base_url'] = 'https://web21facturacion.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
