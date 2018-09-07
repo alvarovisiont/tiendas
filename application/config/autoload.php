2ab1599db1b8238031f44eb5d6b64f9ddf8001ba
sheylert <sheylert@gmail.com>
2018-09-07 15:01:49 -0400

    cambio

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -59,7 +59,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 //$autoload['libraries'] = array('database','session');
-$autoload['libraries'] = array('encrypt', 'calendar', 'form_validation', 'database', 'session', 'xmlrpc', 'upload');
+$autoload['libraries'] = array('calendar', 'form_validation', 'database', 'session', 'xmlrpc', 'upload');
 /*
 | -------------------------------------------------------------------
 |  Auto-load Drivers
