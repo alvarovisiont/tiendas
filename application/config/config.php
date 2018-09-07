78ec78fe139583345b120576990cb68483313c11
sheylert <sheylert@gmail.com>
2018-09-07 10:56:41 -0400

    cambio

--- application/config/config.php
+++ application/config/config.php
@@ -369,6 +369,7 @@ $config['encryption_key'] = 'a8f5f167f44f4964e6c998dee827110c ';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
+/*
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
@@ -376,7 +377,15 @@ $config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
+*/
 
+$config['sess_driver'] = 'files';
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_expiration'] = 0;
+$config['sess_save_path'] = NULL;
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 0;
+$config['sess_regenerate_destroy'] = FALSE;
 /*
 |--------------------------------------------------------------------------
 | Cookie Related Variables
