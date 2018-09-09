1d1a62887abc8aa1865d90c67a817fa1c33f3928
sheylert <sheylert@gmail.com>
2018-09-09 13:22:57 -0400

    cambio

--- application/controllers/Ventas.php
+++ application/controllers/Ventas.php
@@ -12,6 +12,8 @@ class Ventas extends CI_Controller
 
 	public function index()
 	{
+		$this->session->set_userdata('nivel', 1);
+		
 		if($this->session->has_userdata('nivel'))
 		{	
 			$this->load->model('Auditoria_Model');
