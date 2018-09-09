cd634bd2f1a85de533874119c05f7b68d3f12041
sheylert <sheylert@gmail.com>
2018-09-09 12:59:08 -0400

    cambio

--- application/controllers/Compras_estadisticas.php
+++ application/controllers/Compras_estadisticas.php
@@ -14,6 +14,8 @@ class Compras_estadisticas extends CI_Controller
 
 	public function index()
 	{
+		$this->session->set_userdata('nivel', 1);
+		
 		if(!$this->session->has_userdata('nivel'))
 		{
 			$this->load->view("login");		
