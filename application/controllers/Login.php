520ba4579f15de99cd62239beee417b3455b41bd
sheylert <sheylert@gmail.com>
2018-09-11 15:00:47 -0400

    cambio

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -60,9 +60,9 @@ class Login extends CI_Controller
 	public function acceso()
 	{
         /*  ver ..............................*/
-       // $this->session->set_userdata('id', 1);
-	   // $this->session->set_userdata('usuario', 'admin');	
-       // $this->session->set_userdata('nivel', 1);
+       $this->session->set_userdata('id', 1);
+	   $this->session->set_userdata('usuario', 'admin');	
+       $this->session->set_userdata('nivel', 1);
         /* ----------------------------------*/
 
 		if($this->session->userdata('nivel') != NULL)
