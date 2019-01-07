03f370b8cb9f698f05cddbf63e62fa2feb2cceff
Alvaro Guedez <alvaro.develoop@gmail.com>
2019-01-07 17:45:47 -0400

    cambios

--- application/config/config.php
+++ application/config/config.php
@@ -23,6 +23,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+/*$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+if (!isset($_SERVER['ORIG_SCRIPT_NAME']))
+{
+  $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+}
+else
+{
+  $config['base_url'] .= str_replace(basename($_SERVER['ORIG_SCRIPT_NAME']),"",$_SERVER['ORIG_SCRIPT_NAME']);
+}*/
+
 $config['base_url'] = 'https://web21facturacion.herokuapp.com/';
 
 /*
