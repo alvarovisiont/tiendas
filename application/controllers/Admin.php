66032ae355265f1f3130d86fa418d9dd295ebebf
Alvaro Guedez <alvaro.develoop@gmail.com>
2019-01-10 12:11:11 -0400

    cambios

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -30,7 +30,21 @@ class Admin extends CI_Controller
 		}
 		else
 		{
-			$this->load->view("login");
+
+			$this->load->model('Empleados_Model');
+
+			$this->load->model('Auditoria_Model');
+			$this->Auditoria_Model->grabar_ultima_conexion();
+
+			$datos = $this->Admin_Model->traer_datos();
+			$data = $this->Admin_Model->traer_compras_ventas();
+			$empleados = $this->Empleados_Model->traer_datos();
+
+			$this->load->view("encabezado");
+			$this->load->view("admin", compact('datos', 'empleados'));
+			$this->load->view("footer_admin", compact('data'));
+
+			//$this->load->view("login");
 		}
 	}
 }
