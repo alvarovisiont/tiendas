acd8fd80554c8fc5c50cafc4f6c3a242d460048c
Alvaro Guedez <alvaro.develoop@gmail.com>
2019-02-13 01:09:22 -0400

    cambio

--- application/controllers/Usuarios_agregar.php
+++ application/controllers/Usuarios_agregar.php
@@ -5,7 +5,7 @@ class Usuarios_agregar extends CI_Controller
 {
 	public function index()
 	{
-		$this->session->set_userdata('nivel', 1);
+		//$this->session->set_userdata('nivel', 1);
 		
 		if(!$this->session->has_userdata('nivel'))
 		{
