381bb769c56cf9e014078fc25b99a639e1fca311
Alvaro Guedez <alvaro.develoop@gmail.com>
2019-02-16 17:56:06 -0430

    commit

--- application/controllers/Comisiones.php
+++ application/controllers/Comisiones.php
@@ -8,20 +8,67 @@ class Comisiones extends CI_Controller
 	{
           
           parent:: __Construct(); 
-          $array = ['Comision_Model','Auditoria_Model'];
+          $array = ['Comision_Model','Auditoria_Model','Usuarios_Model'];
           $this->load->model($array);
 	}
 
 	public function index(){
 		$data = $this->Comision_Model->get();
 		$datos= $this->Comision_Model->get_total_by_month();
+		$empleados = $this->Usuarios_Model->traer_trabajadores();
 
 		$this->load->model('Auditoria_Model');
 		$this->Auditoria_Model->grabar_ultima_conexion();
 		
+		$select_worker = "<option value=''>Todos</option>";
+		foreach ($empleados as $row) {
+			$select_worker.="<option value='$row->id'>$row->nombre_apellido</option>";
+		}
+
 		$this->load->view("encabezado");
-		$this->load->view("comision", compact('data','datos'));
-		$this->load->view("footer");
+		$this->load->view("comision", compact('data','datos','select_worker'));
+		$this->load->view("footer_comision");
+
+	}
+
+	public function get_registers_by_date(){
+
+		$desde = $this->input->get('desde');
+		$hasta = $this->input->get('hasta');
+		$worker = $this->input->get('worker');
+		$where = null;
+
+		if(!empty($desde)){
+			$where = "( CAST(comision.created_at as DATE) >= '$desde'";
+		}
+
+		if(!empty($hasta)){
+			if(!empty($where)){
+				$where .= " AND CAST(comision.created_at as DATE) <= '$hasta' )";
+			}else{
+				$where = "CAST(comision.created_at as DATE) <= '$hasta'";
+			}
+		}else{
+			if(!empty($where)){
+				$where.= " )";
+			}
+		}
+
+		if(!empty($worker)){
+			if(!empty($where)){
+				$where .= " AND id_empleado = $worker";
+			}else{
+				$where = "id_empleado = $worker";
+			}	
+		}
+
+			
+
+		$data = $this->Comision_Model->get(null,$where);
+		$datos= $this->Comision_Model->get_total_by_month(null,$where);
+
+		echo json_encode(['individual' => $data, 'grupal' => $datos]);
+
 
 	}
 
--- application/views/comision.php
+++ application/views/comision.php
@@ -2,6 +2,10 @@
 	<div class="container-fluid">
 		<div class="col-md-12">
 			<br>
+			<div class="col-md-6 col-sm-6 col-xl-12 col-md-offset-3 col-sm-offset-3">
+				<button class="btn btn-primary btn-block" data-target="#modal_filtros" data-toggle="modal">Filtrar&nbsp;<i class="fa fa-search"></i></button>
+			</div>
+			<br>
 			<br>
 			<br>
 			<div class="panel panel-black">
@@ -19,27 +23,13 @@
 						</thead>
 						<tbody class="text-center">
 							<?php
-								if(!empty($data))
-								{
-									foreach ($data as $row) 
-									{
-										echo "<tr>
-												<td>$row->nombre_apellido</td>
-												<td>$row->factura</td>
-												<td>$row->porcentaje %</td>
-												<td><span class='badge letras' style='background-color: darkred; color: white;'>".number_format($row->monto,2,',','.')." Bs.S</span></td>
-												<td>".date('d-m-Y', strtotime($row->created_at))."</td>
-											</tr>";
-									}
-								}
-								else
-								{
+								foreach ($data as $row) {
 									echo "<tr>
-											<td></td>
-											<td></td>
-											<td></td>
-											<td></td>
-											<td></td>
+											<td>$row->nombre_apellido</td>
+											<td>$row->factura</td>
+											<td>$row->porcentaje %</td>
+											<td><span class='badge letras' style='background-color: darkred; color: white;'>".number_format($row->monto,2,',','.')." Bs.S</span></td>
+											<td>$row->fecha1</td>
 										</tr>";
 								}
 							?>
@@ -52,7 +42,7 @@
 					<h3>Total Pagar por Mes</h3>	
 				</div>
 				<div class="panel-body">
-					<table class="table table-streped table-hover" id="tabla">
+					<table class="table table-streped table-hover" id="tabla_grupal">
 						<thead>
 							<th class="text-center">Trabajador</th>
 							<th class="text-center">Monto</th>
@@ -61,33 +51,58 @@
 						</thead>
 						<tbody class="text-center">
 							<?php
-								if(!empty($datos))
-								{
 
-									foreach ($datos as $row) 
-									{
-										echo "<tr>
-												<td>$row->nombre_apellido</td>
-												<td><span class='badge letras' style='background-color: darkred; color: white;'>".number_format($row->total,2,',','.')."</span></td>
-												<td>".month_return($row->mes)."</td>
-												<td>$row->año</td>
-											</tr>";
-									}
-								}
-								else
+								foreach ($datos as $row) 
 								{
 									echo "<tr>
-											<td></td>
-											<td></td>
-											<td></td>
-											<td></td>
+											<td>$row->nombre_apellido</td>
+											<td><span class='badge letras' style='background-color: darkred; color: white;'>".number_format($row->total,2,',','.')."</span></td>
+											<td>".month_return($row->mes)."</td>
+											<td>$row->año</td>
 										</tr>";
 								}
+								
 							?>
 						</tbody>
 					</table>	
 				</div>
 			</div>
 		</div>
+	</div>
+</div>
+
+<div class="modal fade" id="modal_filtros" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  	<div class="modal-dialog modal-lg" role="document">
+    	<div class="modal-content">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	        <h3 class="text-center">Filtros de Registros</h3>
+	      </div>
+	      <div class="modal-body">
+	      	<div class="row">
+    				<div class="col-md-6">
+    					<label class="control-label">Desde</label>
+							<input type="date" name="desde" id="desde" class="form-control">
+						</div>
+						<div class="col-md-6">
+							<label class="control-label">Hasta</label>
+							<input type="date" name="hasta" id="hasta" class="form-control">
+						</div>  
+						<br/>			
+						<div class="col-md-6">
+							<label class="control-label">Trabajador</label>
+							<select class="form-control" id="worker" name="worker">
+								<?= $select_worker; ?>
+							</select>
+						</div>  			
+	      	</div>
+	      </div>
+	      <div class="modal-footer">
+	      	<button class="btn btn-default btn-md" type="button" data-dismiss="modal">Cerrar&nbsp;<i class="fa fa-remove"></i></button>
+	      	<button class="btn btn-success" id="btn_filter"><span id="span_filter">Fitrar</span>&nbsp;<i class="fa fa-search"></i></button>
+	      </div>
+    	</div>
 	</div>
 </div>
