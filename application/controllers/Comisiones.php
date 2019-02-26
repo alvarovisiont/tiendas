4c0091e78b192e0149b5df6917af8d39b2fd6ce3
Alvaro Guedez <alvaro.develoop@gmail.com>
2019-02-25 22:29:48 -0400

    cambio

--- application/controllers/Comisiones.php
+++ application/controllers/Comisiones.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+require_once __DIR__ . '/../../vendor/autoload.php';
+
 class Comisiones extends CI_Controller 
 {
 
@@ -63,10 +65,16 @@ class Comisiones extends CI_Controller
 		$desde = $this->input->get('desde');
 		$hasta = $this->input->get('hasta');
 		$worker = $this->input->get('worker');
+		$varfechaDesde = "";
+		$varfechaHasta = "";
+
 		$where = null;
 
 		if(!empty($desde)){
 			$where = "( CAST(comision.created_at as DATE) >= '$desde'";
+		
+
+
 		}
 
 		if(!empty($hasta)){
@@ -122,5 +130,79 @@ class Comisiones extends CI_Controller
 
 
 	}
+
+
+	public function pdf(){
+
+		$desde =$this->input->post('desde', TRUE);
+		$hasta = $this->input->post('hasta', TRUE);
+		$worker = $this->input->post('worker', TRUE);
+		$where = null;
+
+		if(!empty($desde)){
+			$where = "( CAST(comision.created_at as DATE) >= '$desde'";
+		}
+
+		if(!empty($hasta)){
+			if(!empty($where)){
+				$where .= " AND CAST(comision.created_at as DATE) <= '$hasta' )";
+			}else{
+				$where = "CAST(comision.created_at as DATE) <= '$hasta'";
+			}
+		}else{
+			if(!empty($where)){
+				$where.= " )";
+			}
+		}
+
+		if(!empty($worker)){
+			if(!empty($where)){
+				$where .= " AND id_empleado = $worker";
+			}else{
+				$where = "id_empleado = $worker";
+			}	
+		}
+
+			
+
+		if($this->session->userdata('nivel') == 1){
+				
+			$data = $this->Comision_Model->get(null,$where);
+			$datos= $this->Comision_Model->get_total_by_month(null,$where);
+			$datos_anulate = $this->Comision_Model->get_total_by_month_anulate(null,$where);
+
+	  }else{
+			$data = $this->Comision_Model->get(null,$where, $this->session->userdata('id'));
+			$datos= $this->Comision_Model->get_total_by_month(null,$where, $this->session->userdata('id'));
+			$datos_anulate = $this->Comision_Model->get_total_by_month_anulate(null,$where, $this->session->userdata('id'));
+
+		}	
+
+		$data_by_month = [];
+
+		foreach ($datos as $row) {
+			
+			foreach ($datos_anulate as $row1) {
+				if(($row->mes === $row1->mes) && ($row->año === $row1->año) && 	($row->id_empleado === $row1->id_empleado)) {
+					$row->total = $row->total - $row1->total;
+				}
+			}
+
+			$data_by_month[] = $row;
+		}
+
+		$html = $this->load->view('imprimir_comisiones_venta', compact('$data_by_month', 'data'), TRUE);
+			
+			$mpdf = new \Mpdf\Mpdf(['mode' => 'utf-8', 'format' => 'A4', [190, 236] ] );
+			$mpdf->WriteHTML($html);
+			$mpdf->Output('Reporte de comisiones.pdf', "I");
+
+		//echo json_encode(['individual' => $data, 'grupal' => $data_by_month]);
+
+    
+	} //fin pdf
+
+
+
 
 }
